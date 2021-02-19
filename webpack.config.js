ae9d07d81e9a2f468e8d250e600ff35e33538921
bachtuyetx7chulun <contact.truongtrunghieu@gmail.com>
2021-02-19 15:40:02 +0700

    u-date

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.min.js",
+  },
+
+  target: "node",
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+
+  plugins: [new CleanWebpackPlugin()],
+  devtool: false,
+};
